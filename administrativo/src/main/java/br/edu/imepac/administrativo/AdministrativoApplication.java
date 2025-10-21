c89d389c7c6ab3fb271a191d33b60db2787d1caf
FreitasEverton <everton.freitas@imepac.edu.br>
2025-10-20 22:18:48 -0300

    Estrutura iniaial da aplicação

---
+++ administrativo/src/main/java/br/edu/imepac/administrativo/AdministrativoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.imepac.administrativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministrativoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdministrativoApplication.class, args);
+    }
+
+}
