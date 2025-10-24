e444cc511ff50aa874b7fa08cd72f7f03c9ba454
mateuscrd <mateuscrodrigues123@gmail.com>
2025-10-23 21:17:22 -0300

    Cririação da classe Especialidadesdto

---
+++ common/src/main/java/br/edu/imepac/common/dtos/Especialidadesdto.java
@@ -0,0 +1,11 @@
+package br.edu.imepac.common.dtos;
+
+public class Especialidadesdto {
+
+    //atributos
+
+    private long id;
+    private String nomeEspecialidade;
+    private String descricao;
+
+}
