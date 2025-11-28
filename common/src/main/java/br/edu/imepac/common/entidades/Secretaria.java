71249190a4da7bb022a3a1746fb4075eca3b0b06
FelipeF27 <felipefmachado2017@yahoo.com>
2025-11-27 21:55:26 -0300

    entity secretaria

--- common/src/main/java/br/edu/imepac/common/entidades/Secretaria.java
+++ common/src/main/java/br/edu/imepac/common/entidades/Secretaria.java
@@ -1,11 +1,11 @@
 package br.edu.imepac.common.entidades;
 
 import br.edu.imepac.common.entidades.Pessoa;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 
 @EqualsAndHashCode(callSuper = true)
@@ -13,11 +13,19 @@ import lombok.NoArgsConstructor;
 @Table(name = "secretarias")
 @Data
 @NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class Secretaria extends Pessoa {
 
+    @Column(nullable = false, unique = true, length = 20)
     private String pis;
 
+    @Column(length = 20)
     private String sexo;
 
+    @Column(length = 20)
     private String rg;
+
+    @OneToOne(mappedBy = "secretaria")
+    private Usuario usuario;
 }
