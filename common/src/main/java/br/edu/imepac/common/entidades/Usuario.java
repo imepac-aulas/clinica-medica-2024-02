f1e744213f75c7d1d0330ccbd6b272de43acb7e4
FreitasEverton <everton.freitas@imepac.edu.br>
2025-11-27 21:48:21 -0300

    add user

---
+++ common/src/main/java/br/edu/imepac/common/entidades/Usuario.java
@@ -0,0 +1,15 @@
+package br.edu.imepac.common.entidades;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
