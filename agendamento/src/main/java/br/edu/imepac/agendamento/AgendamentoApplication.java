ab4bef9e45395cbee77cedee5652516cadc91bf2
FreitasEverton <everton.freitas@imepac.edu.br>
2025-12-07 18:03:12 -0300

    Ajuste - correção atendimento

--- agendamento/src/main/java/br/edu/imepac/agendamento/AgendamentoApplication.java
+++ agendamento/src/main/java/br/edu/imepac/agendamento/AgendamentoApplication.java
@@ -2,9 +2,13 @@ package br.edu.imepac.agendamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class AgendamentoApplication {
+
+@EntityScan("br.edu.imepac.common.entidades")
+@EnableJpaRepositories("br.edu.imepac.agendamento.repositories")public class AgendamentoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AgendamentoApplication.class, args);
